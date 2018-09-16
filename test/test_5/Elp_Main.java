4eecbe31e416841f9936bde97e7b3f445aaff962
huangdayu <huangdayu@github.com>
2018-09-16 11:40:42 +0800

    src and test

---
+++ test/test_5/Elp_Main.java
@@ -0,0 +1,3 @@
+package test_5;
+
+public class Elp_Main {}
---
+++ test/test_5/Function.java
@@ -0,0 +1,17 @@
+package test_5;
+/***
+ * 接口�?
+ * @author Administrator
+ *
+ */
+public interface Function {
+
+    /**
+     * 计算函数�?
+     * 
+     * @param x
+     *            自变�?
+     * @return 函数�?
+     */
+    public double f(double x);
+}
