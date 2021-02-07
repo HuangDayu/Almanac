9c349e5e4cc22581f2f787c7ceb74790cc0a70b5
huangdayu <huangdayu@github.com>
2021-02-07 10:47:46 +0800

    修改文档

--- src/test/java/cn/huangdayu/almanac/test/AlmanacApp.java
+++ src/test/java/cn/huangdayu/almanac/test/AlmanacApp.java
@@ -3,18 +3,17 @@ package cn.huangdayu.almanac.test;
 import cn.huangdayu.almanac.Almanac;
 import cn.huangdayu.almanac.dto.AlmanacDTO;
 import cn.huangdayu.almanac.dto.TimeZoneDTO;
-import cn.huangdayu.almanac.utils.AlmanacUtils;
 
 import java.util.Map;
 
 /**
  * @author huangdayu create at 2021/1/25 10:08
  */
-public class AlmanacTest3  extends Almanac {
+public class AlmanacApp extends Almanac {
 
     public static void main(String[] args) {
-        Almanac almanacTest3 = new AlmanacTest3();
-        AlmanacDTO almanacDTO = almanacTest3.yearCalendar()[7][11];
+        Almanac almanac = new AlmanacApp();
+        AlmanacDTO almanacDTO = almanac.yearCalendar()[7][11];
         TestUtils.pakMap(almanacDTO);
         for (Map.Entry<String, String> entry : TestUtils.MAP.entrySet()) {
             System.out.println(entry.getKey() + entry.getValue());
