48db225620e09f9ceaee845b4109d5766741f2c9
huangdayu <1161946342@qq.com>
2021-11-27 16:58:13 +0800

    重构：迁移节假日几圈

--- src/main/java/cn/huangdayu/almanac/aggregates/holiday/Holiday.java
+++ src/main/java/cn/huangdayu/almanac/aggregates/holiday/Holiday.java
@@ -1,9 +1,25 @@
 package cn.huangdayu.almanac.aggregates.holiday;
 
+import cn.huangdayu.almanac.aggregates.era.Era;
+import cn.huangdayu.almanac.aggregates.lunar.Lunar;
+import cn.huangdayu.almanac.aggregates.solar_term.SolarTerm;
+import cn.huangdayu.almanac.dto.TimeZoneDTO;
+import cn.huangdayu.almanac.utils.AnnalsUtils;
+import cn.huangdayu.almanac.utils.FestivalHolidayUtils;
+import lombok.Data;
+
 /**
  * @author huangdayu create at 2021/1/21 11:11
  */
+@Data
 public class Holiday {
+
+    public Holiday(TimeZoneDTO timeZoneDTO, Lunar lunar, SolarTerm solarTerm, Era era) {
+        // 计算农历节日
+        AnnalsUtils.getHolidayInfo(this, lunar, solarTerm, era);
+        // 计算公历节日
+        FestivalHolidayUtils.getDayName(timeZoneDTO, this);
+    }
 
     /**
      * 重要喜庆日子名称(可将日子名称置红)
@@ -21,38 +37,6 @@ public class Holiday {
      * 放假日子(可用于日期数字置红)
      */
     private int flag;
-
-    public String getHappyDay() {
-        return happyDay;
-    }
-
-    public void setHappyDay(String happyDay) {
-        this.happyDay = happyDay;
-    }
-
-    public String getMajorDay() {
-        return majorDay;
-    }
-
-    public void setMajorDay(String majorDay) {
-        this.majorDay = majorDay;
-    }
-
-    public String getOtherDay() {
-        return otherDay;
-    }
-
-    public void setOtherDay(String otherDay) {
-        this.otherDay = otherDay;
-    }
-
-    public int getFlag() {
-        return flag;
-    }
-
-    public void setFlag(int flag) {
-        this.flag = flag;
-    }
 
     public String getInfo() {
         return getHappyDay() + " " + getMajorDay() + " " + getOtherDay();
