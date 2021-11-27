6c2864690c65d178f21a0df8ef0cf5d2b3c67923
huangdayu <1161946342@qq.com>
2021-11-27 16:16:28 +0800

    重构：迁移节气计算

--- src/main/java/cn/huangdayu/almanac/aggregates/solar_term/SolarTerm.java
+++ src/main/java/cn/huangdayu/almanac/aggregates/solar_term/SolarTerm.java
@@ -1,11 +1,63 @@
 package cn.huangdayu.almanac.aggregates.solar_term;
 
+import cn.huangdayu.almanac.aggregates.astronomical.Astronomical;
+import cn.huangdayu.almanac.aggregates.qishuo.QiShuo;
+import cn.huangdayu.almanac.utils.AnnalsUtils;
+import cn.huangdayu.almanac.utils.CommonUtils;
+import cn.huangdayu.almanac.utils.ConstantsUtils;
+import cn.huangdayu.almanac.utils.JulianCalendarUtils;
+import lombok.Data;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author huangdayu create at 2021/1/22 11:37
  */
+@Data
 public class SolarTerm {
+
+    private SolarTerm() {
+    }
+
+    public SolarTerm(int julianDayForToday, QiShuo qiShuo, Astronomical astronomical) {
+        // 计算当前月份的前一个节气，并从该节气开始结算接下来的24个节气
+        int qk = (int) Math.floor((julianDayForToday - qiShuo.zhongQi[0] - 7) / 15.2184);
+        if (qk < 23 && julianDayForToday >= qiShuo.zhongQi[qk + 1]) {
+            // 节气的取值范围是0-23
+            qk++;
+        }
+        double sunLonValue = astronomical.getSunSolarRetina();
+        double sunLonTime;
+        List<SolarTerm> solarTermAfterDTOS = new ArrayList<>();
+        int qj = 24;
+        int qn;
+        for (int qi = qk; qi < qj; ) {
+            // FIXME 2021-01-17 计算太阳视黄经较耗时
+            sunLonTime = AnnalsUtils.qi_accurate(sunLonValue);
+            julianDay = (int) Math.floor(sunLonTime + 0.5);
+            qn = (int) Math.floor(sunLonValue / CommonUtils.PI_2 * 24 + 24000006.01) % 24;
+            sunLonValue += CommonUtils.PI_2 / 24;
+            // BUG 2021-01-23 qiShuoDO.ZQ[qi]的儒略日与sunLonTime的值有出入,与julianDayOfThisDay的值一致
+            julianDay = CommonUtils.JULIAN_FOR_2000 + julianDay;
+            int afterDays = julianDay - CommonUtils.JULIAN_FOR_2000 - julianDayForToday;
+            SolarTerm solarTerm = afterDays == 0 ? this : new SolarTerm();
+            solarTerm.setJulianTime(sunLonTime);
+            solarTerm.setDateTime(JulianCalendarUtils.julianDays2str(CommonUtils.JULIAN_FOR_2000 + sunLonTime));
+            solarTerm.setIndex(qn);
+            solarTerm.setName(AnnalsUtils.JIEQI[qn]);
+            solarTerm.setJulianDay(julianDay);
+            solarTerm.setDesc(ConstantsUtils.getDesc(AnnalsUtils.JIEQI[qn]));
+            solarTerm.setAfterDay(afterDays);
+            solarTermAfterDTOS.add(solarTerm);
+            qi++;
+            if (qi == 24) {
+                qi = 0;
+                qj = qk;
+            }
+        }
+        this.setNext(solarTermAfterDTOS);
+    }
 
     private Integer index;
     private String name;
@@ -19,71 +71,6 @@ public class SolarTerm {
     private double julianTime;
 
     private List<SolarTerm> next;
-
-    public Integer getIndex() {
-        return index;
-    }
-
-    public void setIndex(Integer index) {
-        this.index = index;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getJulianDay() {
-        return julianDay;
-    }
-
-    public void setJulianDay(Integer julianDay) {
-        this.julianDay = julianDay;
-    }
-
-    public Integer getAfterDay() {
-        return afterDay;
-    }
-
-    public void setAfterDay(Integer afterDay) {
-        this.afterDay = afterDay;
-    }
-
-    public String getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(String dateTime) {
-        this.dateTime = dateTime;
-    }
-
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public void setDesc(String desc) {
-        this.desc = desc;
-    }
-
-    public List<SolarTerm> getNext() {
-        return next;
-    }
-
-    public void setNext(List<SolarTerm> next) {
-        this.next = next;
-    }
-
-    public double getJulianTime() {
-        return julianTime;
-    }
-
-    public void setJulianTime(double julianTime) {
-        this.julianTime = julianTime;
-    }
 
     public SolarTerm getNextOne() {
         for (SolarTerm solarTerm : next) {
--- src/main/java/cn/huangdayu/almanac/utils/SolarTermUtils.java
+++ src/main/java/cn/huangdayu/almanac/utils/SolarTermUtils.java
@@ -1,46 +1,46 @@
-package cn.huangdayu.almanac.utils;
-
-import cn.huangdayu.almanac.aggregates.solar_term.SolarTerm;
-import cn.huangdayu.almanac.dto.TimeZoneDTO;
-
-import java.util.*;
-
-/**
- * 24节气工具类
- *
- * @author huangdayu
- * @update 2020-03-15
- */
-public class SolarTermUtils {
-
-    /***
-     * 定气测试函数
-     *
-     * @param year
-     * @return
-     */
-    @Deprecated
-    private static String[] get24SolarTerm(int year) {
-        String[] solarTerms = new String[24];
-        double T;
-        int y = CommonUtils.year2Ayear(String.valueOf(year)) - 2001;
-        int n = 24;
-        for (int i = 21, index = 0; i < n; ) {
-            T = AstronomyArithmeticUtils.S_aLon_t((y + (double) i * 15 / 360 + 1) * 2 * Math.PI); // 精确节气时间计算
-            String solarTerm = JulianCalendarUtils.julianDays2str(T * 36525 + CommonUtils.JULIAN_FOR_2000 + (double) 8 / 24 - CommonUtils.dtT(T * 36525))
-                    + " " + AnnalsUtils.JIEQI[(i - 18 >= 0 ? i - 18 : i + 6) % 24]; // 日期转为字串
-            solarTerms[index] = solarTerm.trim();
-            i++;
-            index++;
-            if (i == 24) {
-                i = 0;
-                n = 21;
-                y += 1;
-            }
-        }
-        return solarTerms;
-    }
-
+//package cn.huangdayu.almanac.utils;
+//
+//import cn.huangdayu.almanac.aggregates.solar_term.SolarTerm;
+//import cn.huangdayu.almanac.dto.TimeZoneDTO;
+//
+//import java.util.*;
+//
+///**
+// * 24节气工具类
+// *
+// * @author huangdayu
+// * @update 2020-03-15
+// */
+//public class SolarTermUtils {
+//
+//    /***
+//     * 定气测试函数
+//     *
+//     * @param year
+//     * @return
+//     */
+//    @Deprecated
+//    private static String[] get24SolarTerm(int year) {
+//        String[] solarTerms = new String[24];
+//        double T;
+//        int y = CommonUtils.year2Ayear(String.valueOf(year)) - 2001;
+//        int n = 24;
+//        for (int i = 21, index = 0; i < n; ) {
+//            T = AstronomyArithmeticUtils.S_aLon_t((y + (double) i * 15 / 360 + 1) * 2 * Math.PI); // 精确节气时间计算
+//            String solarTerm = JulianCalendarUtils.julianDays2str(T * 36525 + CommonUtils.JULIAN_FOR_2000 + (double) 8 / 24 - CommonUtils.dtT(T * 36525))
+//                    + " " + AnnalsUtils.JIEQI[(i - 18 >= 0 ? i - 18 : i + 6) % 24]; // 日期转为字串
+//            solarTerms[index] = solarTerm.trim();
+//            i++;
+//            index++;
+//            if (i == 24) {
+//                i = 0;
+//                n = 21;
+//                y += 1;
+//            }
+//        }
+//        return solarTerms;
+//    }
+//
 //    /***
 //     * 查询指定节气的日期
 //     * @param solarTermName
@@ -56,44 +56,44 @@ public class SolarTermUtils {
 //        }
 //        return null;
 //    }
-
-
-    /**
-     * 获取该年份的所有节气
-     *
-     * @param year
-     * @return
-     */
-    public static List<SolarTerm> getSolarTermsByYear(int year) {
-        List<SolarTerm> solarTerms = new ArrayList<>();
-        // 计算天文纪年
-        int chronicle = CommonUtils.year2Ayear(String.valueOf(year)) - 2001, n = 24;
-        // 先申明引用，避免反复申请栈空间
-        double sunLonValue, sunLonTime, julianDays;
-        // 从立春开始计算
-        for (int i = 21; i < n; ) {
-            // 计算太阳视黄经
-            sunLonValue = (chronicle + (double) i * 15 / 360 + 1) * 2 * Math.PI;
-            // 计算精确节气时间
-            sunLonTime = AstronomyArithmeticUtils.S_aLon_t(sunLonValue);
-            // 计算气朔
-            julianDays = sunLonTime * 36525 + CommonUtils.JULIAN_FOR_2000 + (double) 8 / 24 - CommonUtils.dtT(sunLonTime * 36525);
-            SolarTerm solarTerm = new SolarTerm();
-            String name = AnnalsUtils.JIEQI[(i - 18 >= 0 ? i - 18 : i + 6) % 24];
-            solarTerm.setName(name);
-            solarTerm.setDesc(ConstantsUtils.getDesc(name));
-            solarTerm.setDateTime(JulianCalendarUtils.julianDays2str(julianDays));
-            solarTerms.add(solarTerm);
-            i++;
-            if (i == 24) {
-                i = 0;
-                n = 21;
-                chronicle += 1;
-            }
-        }
-        return solarTerms;
-    }
-
+//
+//
+//    /**
+//     * 获取该年份的所有节气
+//     *
+//     * @param year
+//     * @return
+//     */
+//    public static List<SolarTerm> getSolarTermsByYear(int year) {
+//        List<SolarTerm> solarTerms = new ArrayList<>();
+//        // 计算天文纪年
+//        int chronicle = CommonUtils.year2Ayear(String.valueOf(year)) - 2001, n = 24;
+//        // 先申明引用，避免反复申请栈空间
+//        double sunLonValue, sunLonTime, julianDays;
+//        // 从立春开始计算
+//        for (int i = 21; i < n; ) {
+//            // 计算太阳视黄经
+//            sunLonValue = (chronicle + (double) i * 15 / 360 + 1) * 2 * Math.PI;
+//            // 计算精确节气时间
+//            sunLonTime = AstronomyArithmeticUtils.S_aLon_t(sunLonValue);
+//            // 计算气朔
+//            julianDays = sunLonTime * 36525 + CommonUtils.JULIAN_FOR_2000 + (double) 8 / 24 - CommonUtils.dtT(sunLonTime * 36525);
+//            SolarTerm solarTerm = new SolarTerm();
+//            String name = AnnalsUtils.JIEQI[(i - 18 >= 0 ? i - 18 : i + 6) % 24];
+//            solarTerm.setName(name);
+//            solarTerm.setDesc(ConstantsUtils.getDesc(name));
+//            solarTerm.setDateTime(JulianCalendarUtils.julianDays2str(julianDays));
+//            solarTerms.add(solarTerm);
+//            i++;
+//            if (i == 24) {
+//                i = 0;
+//                n = 21;
+//                chronicle += 1;
+//            }
+//        }
+//        return solarTerms;
+//    }
+//
 //    /**
 //     * 获取最近节气
 //     *
@@ -121,5 +121,5 @@ public class SolarTermUtils {
 //        list.addAll(getSolarTermsByYear(timeZoneDTO.getYear() + 1));
 //        return getSolarTerm(current, list, timeZoneDTO);
 //    }
-
-}
+//
+//}
